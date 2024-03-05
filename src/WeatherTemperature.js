3f1a51d35cc8d50b970717bad91abfa2aa71da01
Joey199-sudo <94124588+Joey199-sudo@users.noreply.github.com>
2024-03-05 16:08:30 +0200

    added the conversion of degrees

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">&deg;C | &deg; F</span>
-    </div>
-  );
+    const [unit, setUnit] = useState('celsius');
+
+    function convertToFahrenheit(event) {
+        event.preventDefault();
+        setUnit('fahrenheit');
+    }
+
+    function convertToCelsius(event) {
+        event.preventDefault();
+        setUnit('celsius');
+    }
+
+    function convertTemperature() {
+        if (unit === 'celsius') {
+            return Math.round(props.celsius) ;
+        } else {
+            return Math.round((props.celsius * 9) / 5 + 32);
+        }
+    }
+
+    return (
+        <div className="WeatherTemperature">
+            <span className="temperature">{convertTemperature()}</span>
+            <span className="unit">
+                {unit === 'celsius' ? (
+                    <>
+                        | <a href="/" onClick={convertToFahrenheit}>
+                            °F
+                        </a>
+                    </>
+                ) : (
+                    <>
+                        | <a href="/" onClick={convertToCelsius}>
+                            °C
+                        </a>
+                    </>
+                )}
+            </span>
+        </div>
+    );
 }
