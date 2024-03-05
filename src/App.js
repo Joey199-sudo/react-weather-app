46d35912fb8040e07485136c36bd684434f53e7c
Joey199-sudo <94124588+Joey199-sudo@users.noreply.github.com>
2024-03-05 12:37:12 +0200

    Added basic styling with bootstrap npm

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className='container'>
-      <h1>Weather App</h1>
       
+      <Weather/>
       <footer>
-        This project is coded by <a href="https://github.com/Joey199-sudo" target='_blank' rel="noreferrer" >Johannah Makgato</a> and is open sourced{" "}
+        This project is coded by 
+        <a href="https://github.com/Joey199-sudo" target='_blank' rel="noreferrer" >
+          Johannah Makgato
+        </a> 
+        and is{" "}
 
       <a href="https://github.com/Joey199-sudo/react-weather-app"
         target='_blank'
-        rel="noreferrer" 
-
-      >open-sourced on GitHub</a>{" "}
-      can also be found on <a href="https://silly-fairy-ddc531.netlify.app/" target='_blank' rel="noreferrer" >netlify</a>
+        rel="noreferrer">
+          open-sourced on GitHub
+        </a>{" "}
+      can also be found on {" "}
+      <a href="https://silly-fairy-ddc531.netlify.app/" target='_blank' rel="noreferrer" >
+          netlify
+      </a>
       </footer>
       </div>
     </div>
