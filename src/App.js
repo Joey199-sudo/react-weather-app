b892de5fe63d44e709b54509a1ab6028c81c1bee
Joey199-sudo <94124588+Joey199-sudo@users.noreply.github.com>
2024-03-05 09:16:58 +0200

    Added netlify and github urls

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by <a href="https://github.com/Joey199-sudo" target='_blank'>Johannah Makgato</a> and is open sourced{" "}
+
+      <a href="https://github.com/Joey199-sudo/react-weather-app"
+        target='_blank'
+
+      >open-sourced on GitHub</a>{" "}
+      can also be found on <a href="https://silly-fairy-ddc531.netlify.app/" target='_blank'>netlify</a>
+      </footer>
     </div>
   );
 }
