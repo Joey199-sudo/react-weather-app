bebee8f313c1a1cbb42e749a67f19302083ed9a2
Joey199-sudo <94124588+Joey199-sudo@users.noreply.github.com>
2024-03-05 09:56:04 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <h1>Weather App</h1>
+      
       <footer>
         This project is coded by <a href="https://github.com/Joey199-sudo" target='_blank' rel="noreferrer" >Johannah Makgato</a> and is open sourced{" "}
 
@@ -15,6 +17,7 @@ export default function App() {
       >open-sourced on GitHub</a>{" "}
       can also be found on <a href="https://silly-fairy-ddc531.netlify.app/" target='_blank' rel="noreferrer" >netlify</a>
       </footer>
+      </div>
     </div>
   );
 }
